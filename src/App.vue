0709ea5d112c4e187630cdb2e376033b1e713e77
Isaac <isaacfelipee3@gmail.com>
2024-09-22 17:09:53 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-slate-50 min-h-screen font-sans">
     <!-- Header -->
-    <header class="bg-blue-500 p-4 text-white flex text-center justify-around h-16">
+    <header class="bg-cyan-700 p-4 text-white flex flex-col md:flex-row text-center justify-around h-auto">
       <a aria-label="Ir para a página inicial" href="/">
         <img src="./assets/icons/logo.svg" class="w-[2em] mx-5 invert" alt="">
       </a>
       <nav>
-        <ul class="flex justify-between gap-4 mx-7 mt-1 text-lg font-semibold">
+        <ul class="flex flex-col md:flex-row justify-center gap-4 mt-1 text-lg font-semibold">
           <li><a href="#sobre" class="hover:text-sky-700">Sobre mim</a></li>
           <li><a href="#projetos" class="hover:text-sky-700">Projetos</a></li>
           <li><a href="#contato" class="hover:text-sky-700">Contato</a></li>
@@ -16,61 +16,63 @@
 
     <!-- Seção 'Sobre mim' -->
     <main id="sobre" class="min-h-screen w-auto">
-      <div class="bg-blue-500 flex justify-around items-center text-center h-[42em]">
-        <section class="px-[12em] flex flex-col text-start text-white font-extrabold">
+      <div class="bg-cyan-700 flex flex-col md:flex-row justify-around items-center text-center h-auto md:h-[42em]">
+        <section class="px-5 md:px-[12em] flex flex-col text-start text-white font-extrabold">
           <p class="text-3xl">Olá!</p>
-          <p class="text-7xl">Eu sou Isaac</p>
+          <p class="text-5xl md:text-7xl">Eu sou Isaac</p>
           <p class="font-bold">
             Seu resolvedor de problemas.
           </p>
         </section>
         <article>
-          <img src="./assets/avatar_original.png" alt="">
+          <img src="./assets/avatar_original.png" alt="" class="w-32 md:w-auto"/>
         </article>
       </div>
-      <div class="flex justify-between p-10 px-[10rem] divide-x divide-x-2 divide-slate-400 font-semibold">
-        <article class="flex flex-row flex-col px-5">
+      <div class="flex flex-col md:flex-row justify-between p-10 px-5 md:px-[10rem] divide-x divide-x-2 divide-slate-400 font-semibold">
+        <article class="flex flex-col px-5 mb-5 md:mb-0">
           <p>Bem-vindo ao meu mundo! Sou um desenvolvedor full stack apaixonado por criar soluções inovadoras e funcionais. Estou aqui para transformar necessidades em experiências excepcionais.</p>
-          <div class="flex gap-4 pt-5">
+          <div class="flex gap-4 pt-5 flex-wrap">
             <button
             @click="baixarCurriculo"
-            class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-[5rem] gap-2 rounded-lg transition duration-300 ease-in-out shadow-xl flex"
+            class="bg-cyan-700 hover:bg-cyan-600 text-white font-bold py-3 px-4 md:px-[5rem] gap-2 rounded-lg transition duration-300 ease-in-out shadow-xl flex items-center"
             >
               <img src="./assets/icons/curriculo.svg" alt="Curriculo" class="w-4 h-4 mt-1 invert" />
               Meu currículo
             </button>
             <button
             @click="abrirGithub"
-            class="border border-blue-500 border-2 hover:bg-blue-500 hover:text-white font-bold py-3 px-[5rem] rounded-lg transition duration-300 ease-in-out shadow-xl flex"
+            class="border border-cyan-500 border-2 hover:bg-cyan-700 hover:text-white font-bold py-3 px-4 md:px-[5rem] rounded-lg transition duration-300 ease-in-out shadow-xl flex items-center"
             >
               <img src="./assets/icons/github.svg" alt="Github" class="w-5 h-5 mx-1" />
               Github
             </button>
           </div>
         </article>
-        <article class="flex flex-row px-5">
+        <article class="flex flex-col px-5">
           <p>Meu objetivo é criar experiências digitais que não apenas impressionem, mas também funcionem de forma excepcional e agreguem valor a negócios e projetos ao redor do mundo.</p>
         </article>
       </div>
     </main>
 
     <!-- Seção 'Projetos' -->
-    <section id="projetos" class="h-[35em]">
+    <section id="projetos" class="p-10">
       <h2 class="text-3xl font-bold text-center">Projetos</h2>
-      <div class="mt-6 grid grid-cols-1 md:grid-cols-3 gap-6 p-10">
+      <div class="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         <!-- Projeto 1 -->
         <div class="p-6 border rounded-lg bg-gray-50 shadow-xl">
           <h3 class="text-xl font-bold">Projeto 1</h3>
           <p class="mt-2">
-            Descrição curta do projeto.{' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed vero, minus impedit repellendus id a sint voluptatum voluptate autem ducimus culpa. Laboriosam amet excepturi voluptas consequatur perferendis quaerat repellendus facilis?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam suscipit totam magni consectetur labore in voluptatem voluptate voluptas neque corporis veritatis molestias recusandae, accusamus, deserunt laborum. Repellat excepturi esse dignissimos? '}
+            Descrição curta do projeto.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nihil atque ab magnam nobis est! Aliquam quia hic commodi nihil molestias error eos dignissimos, tempore laudantium earum voluptates! Expedita, aliquid.
             <a href="#" class="text-blue-500 underline">Veja mais</a>
           </p>
         </div>
         <!-- Projeto 2 -->
-        <div class="p-10 border rounded-lg bg-gray-50 shadow-xl">
+        <div class="p-6 border rounded-lg bg-gray-50 shadow-xl">
           <h3 class="text-xl font-bold">Projeto 2</h3>
           <p class="mt-2">
-            Descrição curta do projeto.{' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed vero, minus impedit repellendus id a sint voluptatum voluptate autem ducimus culpa. Laboriosam amet excepturi voluptas consequatur perferendis quaerat repellendus facilis?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam suscipit totam magni consectetur labore in voluptatem voluptate voluptas neque corporis veritatis molestias recusandae, accusamus, deserunt laborum. Repellat excepturi esse dignissimos? '}
+            Descrição curta do projeto.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quasi et optio nisi odio cupiditate recusandae dolor culpa saepe maiores voluptates, explicabo reprehenderit cum aspernatur vero possimus nesciunt adipisci consequuntur?
             <a href="#" class="text-blue-500 underline">Veja mais</a>
           </p>
         </div>
@@ -78,24 +80,24 @@
         <div class="p-6 border rounded-lg bg-gray-50 shadow-xl">
           <h3 class="text-xl font-bold">Projeto 3</h3>
           <p class="mt-2">
-            Descrição curta do projeto.{' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed vero, minus impedit repellendus id a sint voluptatum voluptate autem ducimus culpa. Laboriosam amet excepturi voluptas consequatur perferendis quaerat repellendus facilis?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam suscipit totam magni consectetur labore in voluptatem voluptate voluptas neque corporis veritatis molestias recusandae, accusamus, deserunt laborum. Repellat excepturi esse dignissimos? '}
+            Descrição curta do projeto.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae inventore harum enim accusantium modi, ipsam dolore natus sint nam eveniet dolor possimus? Iure dicta est voluptas asperiores, eveniet eos.
             <a href="#" class="text-blue-500 underline">Veja mais</a>
           </p>
         </div>
-        <!-- Futuros projetos aqui -->
       </div>
     </section>
 
     <!-- Seção 'Contato' -->
-    <section id="contato" class="h-[30em]">
-      <div class="container mx-auto px-10">
+    <section id="contato" class="py-8">
+      <div class="container mx-auto px-5">
         <h2 class="text-3xl font-bold text-center mb-6">Contato</h2>
         
-        <div class="p-6 mx-10 gap-5 border rounded-lg bg-gray-50 shadow-xl flex flex-col items-center">
+        <div class="p-6 mx-auto gap-5 border rounded-lg bg-gray-50 shadow-xl flex flex-col items-center">
           <!-- LinkedIn -->
           <div class="flex items-center mb-4">
             <img src="./assets/icons/linkedin.svg" alt="LinkedIn" class="w-6 h-6 mr-2" />
-            <button @click="abrirLinkedIn" class="text-lg text-gray-800 hover:text-blue-500">
+            <button @click="abrirLinkedIn" class="text-lg text-gray-800 hover:text-cyan-500">
               LinkedIn
             </button>
           </div>
@@ -103,7 +105,7 @@
           <!-- Instagram -->
           <div class="flex items-center mb-4">
             <img src="./assets/icons/instagram.svg" alt="Instagram" class="w-6 h-6 mr-2" />
-            <button @click="abrirInstagram" class="text-lg text-gray-800 hover:text-blue-500">
+            <button @click="abrirInstagram" class="text-lg text-gray-800 hover:text-cyan-500">
               Instagram
             </button>
           </div>
@@ -111,7 +113,7 @@
           <!-- Email -->
           <div class="flex items-center mb-4">
             <img src="./assets/icons/email.svg" alt="Email" class="w-6 h-6 mr-2" />
-            <button @click="abrirEmail" class="text-lg text-gray-800 hover:text-blue-500">
+            <button @click="abrirEmail" class="text-lg text-gray-800 hover:text-cyan-500">
               dev.isaaclvs@gmail.com
             </button>
           </div>
@@ -119,7 +121,7 @@
           <!-- WhatsApp -->
           <div class="flex items-center mb-4">
             <img src="./assets/icons/whatsapp.svg" alt="WhatsApp" class="w-6 h-6 mr-2" />
-            <button @click="abrirWhatsApp" class="text-lg text-gray-800 hover:text-blue-500">
+            <button @click="abrirWhatsApp" class="text-lg text-gray-800 hover:text-cyan-500">
               WhatsApp
             </button>
           </div>
@@ -136,7 +138,7 @@
     </section>
 
     <!-- Rodape da pagina -->
-    <footer class="bg-gradient-to-b from-blue-600 to-blue-500 text-white py-6 flex flex-col justify-between items-center space-y-6">
+    <footer class="bg-gradient-to-b from-cyan-700 bg-cyan-600 text-white py-6 flex flex-col justify-between items-center space-y-6">
       <div class="max-w-3xl px-4 flex items-center justify-between space-x-6 mt-10">
         <p class="font-semibold text-center">
           Fico feliz que tenha chegado até aqui, espero que tenha gostado de saber um pouco sobre mim. 
